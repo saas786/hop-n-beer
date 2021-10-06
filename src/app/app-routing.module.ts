11aa5a89170bd5f0cf58ff007c235b7075812325
Stefano Squillante <stefano.squillante7423@gmail.com>
2021-10-06 16:38:58 +0200

    splashscreen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'splashscreen',
+    loadChildren: () => import('./pages/splashscreen/splashscreen.module').then( m => m.SplashscreenPageModule)
   }
 ];
 
