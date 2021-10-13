6adb328040ba16b5a4fa1f45b11858fa7623f445
Stefano Squillante <stefano.squillante7423@gmail.com>
2021-10-13 12:59:13 +0200

    toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'pages/splashscreen',
+    redirectTo: 'pages/home',
     pathMatch: 'full'
   },
   {
